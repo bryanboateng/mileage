e421db96003764184f02a57f3080a0779ad42675
Bryan Oppong-Boateng <bryan@bryanboateng.com>
2022-12-17 23:21:51 +0100

    Initialize app

---
+++ Mileage/MileageApp.swift
@@ -0,0 +1,17 @@
+//
+//  MileageApp.swift
+//  Mileage
+//
+//  Created by Bryan Oppong-Boateng on 17.12.22.
+//
+
+import SwiftUI
+
+@main
+struct MileageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
